e13af0d3c4824f5eb4e1c532002b6b50e114dffc
nwilde <nathan.wilde1994@gmail.com>
2022-10-13 00:08:04 +1100

    Basic collision example

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # CSCI262-A3
+
+Credit to https://github.com/corkami/collisions for the PNG collision code
+
+Pre-requisites: install python, have python in your PATH variable
+
+Usage: `python main.py file1.png file2.png`
+
+Output: MD5 hash and filesize of the original and post collision files
+
+dog.png and fish.png have been provided for easy testing
---
+++ main.py
@@ -0,0 +1,40 @@
+import subprocess
+import sys
+from hashlib import md5, sha256
+#from png import png_coll
+
+
+def main():
+    print("PNG Hash Collision Demo")
+
+    fn1, fn2 = sys.argv[1:3]
+    with open(fn1, "rb") as f:
+        d1 = f.read()
+    with open(fn2, "rb") as f:
+        d2 = f.read()
+
+    d1_hash = md5(d1).hexdigest()
+    d2_hash = md5(d2).hexdigest()
+    print("%s original MD5 hash: %s" % (fn1, d1_hash))
+    print("%s original filesize: %s kB" % (fn1, round(len(d1) / 1024)))
+    print("%s original MD5 hash: %s" % (fn2, d2_hash))
+    print("%s original filesize: %s kB" % (fn1, round(len(d2) / 1024)))
+
+    print("Colliding Files...")
+    subprocess.run(["python", "png.py", fn1, fn2])
+
+    with open("collision1.png", "rb") as f:
+        coll_d1 = f.read()
+    with open("collision2.png", "rb") as f:
+        coll_d2 = f.read()
+
+    coll_d1_hash = md5(coll_d1).hexdigest()
+    coll_d2_hash = md5(coll_d2).hexdigest()
+    print("%s collided MD5 hash: %s" % (fn1, coll_d1_hash))
+    print("%s collided filesize: %s kB" % (fn1, round(len(coll_d1) / 1024)))
+    print("%s collided MD5 hash: %s" % (fn2, coll_d2_hash))
+    print("%s collided filesize: %s kB" % (fn1, round(len(coll_d2) / 1024)))
+
+
+if __name__ == "__main__":
+    main()

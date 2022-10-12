45f3ba0167314a020ebc25547af2336d9b3fe321
nwilde <nathan.wilde1994@gmail.com>
2022-10-13 00:14:59 +1100

    Added SHA-256 of MD% colliding files

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def main():
 
     d1_hash = md5(d1).hexdigest()
     d2_hash = md5(d2).hexdigest()
-    print("%s original MD5 hash: %s" % (fn1, d1_hash))
+    print("%s original file MD5 hash: %s" % (fn1, d1_hash))
     print("%s original filesize: %s kB" % (fn1, round(len(d1) / 1024)))
-    print("%s original MD5 hash: %s" % (fn2, d2_hash))
+    print("%s original file MD5 hash: %s" % (fn2, d2_hash))
     print("%s original filesize: %s kB" % (fn1, round(len(d2) / 1024)))
 
     print("Colliding Files...")
@@ -30,10 +30,16 @@ def main():
 
     coll_d1_hash = md5(coll_d1).hexdigest()
     coll_d2_hash = md5(coll_d2).hexdigest()
-    print("%s collided MD5 hash: %s" % (fn1, coll_d1_hash))
+    print("%s collided file MD5 hash: %s" % (fn1, coll_d1_hash))
     print("%s collided filesize: %s kB" % (fn1, round(len(coll_d1) / 1024)))
-    print("%s collided MD5 hash: %s" % (fn2, coll_d2_hash))
+    print("%s collided file MD5 hash: %s" % (fn2, coll_d2_hash))
     print("%s collided filesize: %s kB" % (fn1, round(len(coll_d2) / 1024)))
+
+    print("Same files that cause MD5 collision hashed with SHA-256:")
+    coll_d1_sha_hash = sha256(coll_d1).hexdigest()
+    coll_d2_sha_hash = sha256(coll_d2).hexdigest()
+    print("%s collided file SHA-256 hash: %s" % (fn1, coll_d1_sha_hash))
+    print("%s collided file SHA-256 hash: %s" % (fn2, coll_d2_sha_hash))
 
 
 if __name__ == "__main__":
